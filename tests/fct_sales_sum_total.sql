ef09c3389577e87127bbd61a1dde66eecf5095c4
Lorenzo Garibotti Sfredo <cubenada@gmail.com>
2023-09-05 06:19:41 +0000

    deletei uma staging descenessaria.

--- tests/fct_sales_sum_total.sql
+++ tests/fct_sales_sum_total.sql
@@ -1,9 +1,9 @@
 with 
     sum_total as (
         select
-            round(sum(total), 2) as total
+            round(sum(subtotal), 2) as total
         from {{ ref('fct_sales') }}
-        where order_date between '2011-01-01' and '2012-01-01'
+        where order_date between '2011-01-01' and '2011-12-31'
     )
 
 select * from sum_total where total != 12646112.16
