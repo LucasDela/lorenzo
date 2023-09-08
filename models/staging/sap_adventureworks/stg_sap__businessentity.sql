556ee2a46225d5e3abe622ba95b9d35c39b5460e
Lorenzo Garibotti Sfredo <cubenada@gmail.com>
2023-09-08 10:07:03 +0000

    mais ajustes finais

--- models/staging/sap_adventureworks/stg_sap__businessentity.sql
+++
@@ -1,8 +0,0 @@
-with
-    source_businessentity as (
-        select
-            cast(businessentityid as int) as client_id
-        from {{ source('sap', 'businessentity') }}
-    )
-select *
-from source_businessentity
